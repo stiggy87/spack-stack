1108bb9881f64aab391e327cf62a0ed79f8d8cfb
Steve Grace <sgrace@ucar.edu>
2025-01-07 11:01:57 -0700

    Initial commit for updating R2D2 to v3

--- spack-ext/repos/spack-stack/packages/r2d2/package.py
+++ spack-ext/repos/spack-stack/packages/r2d2/package.py
@@ -9,16 +9,18 @@ from spack.package import *
 class R2d2(PythonPackage):
     """Research Repository for Data and Diagnostics (R2D2) for JEDI at JCSDA."""
 
-    homepage = "https://github.com/JCSDA/r2d2"
-    git = "https://github.com/JCSDA/r2d2.git"
-    url = "https://github.com/JCSDA/r2d2/archive/refs/tags/1.0.0.tar.gz"
+    homepage = "https://github.com/JCSDA-internal/r2d2-client"
+    git = "https://github.com/JCSDA-internal/r2d2-client.git"
+    url = "https://github.com/JCSDA-internal/r2d2-client/archive/refs/heads/feature/restapi.zip"
 
     maintainers("climbfuji", "ericlingerfelt")
 
-    version("develop", branch="develop", no_cache=True)
-    version("1.0.0", commit="74236aba6817e8ca7880f8109268aae1048ccdbe", preferred=True)
-    version("0.0.1", commit="011990d36c9c651593e5e158b5ad7ef07aee16dc")
+    version("restapi", branch="feature/restapi", no_cache=True)
 
-    depends_on("python@3.7:", type=("build", "run"))
-    depends_on("py-pyyaml@5.3.1:", type=("build", "run"))
-    depends_on("py-boto3", type=("build", "run"))
+    depends_on("python@3.9:", type=("run"))
+    depends_on("py-pyyaml", type=("run"))
+    depends_on("py-boto3", type=("run"))
+    depends_on("py-charset-normalizer", type=("run"))
+    depends_on("py-idna", type=("run"))
+    depends_on("py-requests", type=("run"))
+    depends_on("py-s3transfer", type=("run"))
